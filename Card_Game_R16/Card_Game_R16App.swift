2488d7cc7af4bc32212d680431ccdcaeefde4baa
iSAD <bradx2713@gmail.com>
2024-07-28 07:12:46 +0200

    Initial Commit

---
+++ Card_Game_R16/Card_Game_R16App.swift
@@ -0,0 +1,17 @@
+//
+//  Card_Game_R16App.swift
+//  Card_Game_R16
+//
+//  Created by Tafadzwa Alexander on 2024/07/28.
+//
+
+import SwiftUI
+
+@main
+struct Card_Game_R16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
